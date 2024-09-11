cb7a1621489f59ea2875353cfa20fa270561363c
sebitse <sebastian.tonu14@gmail.com>
2024-09-11 18:50:07 +0300

    implement Singleton and basic key management logic

--- src/main/java/com/simulator/keymanagement/KeyManager.java
+++ src/main/java/com/simulator/keymanagement/KeyManager.java
@@ -1,4 +1,31 @@
 package com.simulator.keymanagement;
 
+import javax.crypto.SecretKey;
+import java.security.KeyPair;
+
 public class KeyManager {
+    private static KeyManager instance;
+    private SecretKey secretKey; // For symmetric algorithms (AES)
+    private KeyPair keyPair;     // For asymmetric algorithms (RSA)
+
+    // private constructor to implement Singleton
+    private KeyManager() {}
+
+
+    public static synchronized KeyManager getInstance() {
+        if(instance == null) {
+            instance = new KeyManager();
+        }
+        return instance;
+    }
+
+
+    // Getters area
+    public SecretKey getSecretKey() {
+        return secretKey;
+    }
+
+    public KeyPair getKeyPair() {
+        return keyPair;
+    }
 }
