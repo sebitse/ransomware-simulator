bd8a480b55ca33541864524833ac2884b29e6f8c
sebitse <sebastian.tonu14@gmail.com>
2024-09-17 01:07:19 +0300

    finish the RSA encryption class

--- src/main/java/com/simulator/encryption/RSAEncryption.java
+++ src/main/java/com/simulator/encryption/RSAEncryption.java
@@ -3,6 +3,7 @@ package com.simulator.encryption;
 
 import javax.crypto.Cipher;
 import java.io.File;
+import java.nio.file.Files;
 import java.security.Key;
 import java.security.PrivateKey;
 import java.security.PublicKey;
@@ -18,10 +19,12 @@ public class RSAEncryption implements EncryptionStrategy {
             throw new IllegalArgumentException("Invalid key type for RSA encryption. Expected PublicKey.");
         }
 
-        Cipher cipher = Cipher.getInstance("RSA");
+        Cipher cipher = Cipher.getInstance(algorithmUsed);
         cipher.init(Cipher.ENCRYPT_MODE, key);  // Use the public key to encrypt
 
-        // to be continued
+        byte[] fileBytes = Files.readAllBytes(file.toPath());
+        byte[] encryptedBytes = cipher.doFinal(fileBytes);
+        Files.write(file.toPath(), encryptedBytes);  // Save encrypted bytes back to the file
     }
 
     @Override
@@ -31,5 +34,11 @@ public class RSAEncryption implements EncryptionStrategy {
             throw new IllegalArgumentException("Invalid key type for RSA decryption. Expected PrivateKey.");
         }
 
+        Cipher cipher = Cipher.getInstance(algorithmUsed);
+        cipher.init(Cipher.DECRYPT_MODE, key);  // Use the private key to decrypt
+
+        byte[] fileBytes = Files.readAllBytes(file.toPath());
+        byte[] decryptedBytes = cipher.doFinal(fileBytes);
+        Files.write(file.toPath(), decryptedBytes);  // save decrypted bytes
     }
 }
