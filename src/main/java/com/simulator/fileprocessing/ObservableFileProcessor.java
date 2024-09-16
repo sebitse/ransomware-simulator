b5d77e253504149023e8f703c1ad1b3f1653748d
sebitse <sebastian.tonu14@gmail.com>
2024-09-17 00:53:23 +0300

    create ObservableFileProcessor to perform the FileProcessor job + notify method

--- src/main/java/com/simulator/fileprocessing/ObservableFileProcessor.java
+++ src/main/java/com/simulator/fileprocessing/ObservableFileProcessor.java
@@ -8,4 +8,21 @@ import java.util.List;
 public abstract class ObservableFileProcessor extends FileProcessor {
     // I want to implement Observer Design Pattern here
     private final List<Observer> observers = new ArrayList<>();
+
+    // add observers
+    public void addObserver(Observer observer) {
+        observers.add(observer);
+    }
+
+    // Delete the observer from the List
+    public void removeObserver(Observer observer) {
+        observers.remove(observer);
+    }
+
+    // Notify all the observers
+    protected void notify(String message) {
+        for (Observer observer : this.observers) {
+            observer.update(message);
+        }
+    }
 }
