3afd114c0f3914fd5c8d457bc88bcabff893a47d
sebitse <sebastian.tonu14@gmail.com>
2024-09-18 21:37:42 +0300

    .lua sample file added

---
+++ data/sample/sample4.lua
@@ -0,0 +1,17 @@
+
+  -- THIS IS JUST A SAMPLE OF LUA, DO NOT TAKE TOO SERIOUS
+  print("Hello World")
+
+
+  -- defines a factorial function
+  function fact (n)
+	  if n == 0 then
+		  return 1
+	  else
+		  return n * fact(n-1)
+	  end
+  end
+
+  print("enter a number:")
+  a = io.read("*number")        -- read a number
+  print(fact(a))
