5e3a1ac17924cd41cb64819421dcc8fdb5c1b1fa
sebitse <sebastian.tonu14@gmail.com>
2024-09-18 21:54:01 +0300

    finish the main loop to run the app

--- src/main/java/com/simulator/RansomwareSimulator.java
+++ src/main/java/com/simulator/RansomwareSimulator.java
@@ -1,6 +1,5 @@
 package com.simulator;
 
-
 public class RansomwareSimulator {
     public static void main(String[] args) {
         RansomwareSimulatorApp app = new RansomwareSimulatorApp();
--- src/main/java/com/simulator/RansomwareSimulatorApp.java
+++ src/main/java/com/simulator/RansomwareSimulatorApp.java
@@ -1,19 +1,45 @@
 package com.simulator;
 
-import com.simulator.keymanagement.KeyManager;
 
-import java.util.Scanner;
+import java.io.File;
+
 
 public class RansomwareSimulatorApp {
-    private final Scanner scanner;
-    private final KeyManager keyManager;
+
+    private final UserInputHandler userInputHandler;
+    private final DirectoryProcessor directoryProcessor;
 
     public RansomwareSimulatorApp() {
-        this.scanner = new Scanner(System.in);
-        this.keyManager = KeyManager.getInstance();
+        this.userInputHandler = new UserInputHandler();
+        this.directoryProcessor = new DirectoryProcessor();
     }
 
+    // Main loop for running the app
     public void run() {
         System.out.println("Ransomware Simulator - Educational Purposes Only");
+
+        boolean running = true;
+        while (running) {
+            int operation = userInputHandler.selectOperation();
+            String algorithm = userInputHandler.selectAlgorithm();
+            File directory = userInputHandler.selectFile();
+
+            try {
+                if (directory.isDirectory()) {
+                    // Delegate directory processing to DirectoryProcessor
+                    directoryProcessor.processDirectory(directory, operation, algorithm);
+                } else {
+                    // Delegate single file processing to DirectoryProcessor
+                    directoryProcessor.processSingleFile(directory, operation, algorithm);
+                }
+            } catch (Exception e) {
+                System.err.println("Error during encryption/decryption: " + e.getMessage());
+            }
+
+            // Ask the user if they want to continue or exit
+            running = userInputHandler.askToContinue();
+        }
+
+        System.out.println("Exiting the Ransomware Simulator. Goodbye!");
     }
 }
